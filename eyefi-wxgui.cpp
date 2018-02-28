c6a511cb53ffe9103c63f37924be302645f0bee0
Bradley Bell <bradley.bell@gmail.com>
2018-02-28 01:48:04 -0800

    finish tree with all basic info

--- eyefi-wxgui.cpp
+++ eyefi-wxgui.cpp
@@ -5,6 +5,8 @@
 //  Created by Bradley Bell on 2/25/18.
 //
 
+#include <cstring>
+
 // For compilers that don't support precompilation, include "wx/wx.h";
 #include "wx/wxprec.h"
 
@@ -32,6 +34,17 @@ extern "C" {
 		exit(1);
 	}
 	
+}
+
+const char *transfer_mode_names[] = {
+	"AUTO",
+	"SELSHARE",
+	"SELUPLOAD",
+};
+
+int pstrcpy(char *dest, pascal_string *src)
+{
+	return snprintf(dest, src->length, "%s", src->value);
 }
 
 class EyeFiGui : public wxApp
@@ -87,7 +100,7 @@ bool EyeFiGui::OnInit()
 	
 	treeList = new wxTreeListCtrl(frame, wxID_ANY);
 	treeList->AppendColumn("Item");
-	treeList->AppendColumn("Value");
+	treeList->AppendColumn("Value", 240);
 
 	frame->Show(true);
 	SetTopWindow(frame);
@@ -95,22 +108,74 @@ bool EyeFiGui::OnInit()
 	char *mountPoint = locate_eyefi_mount();
 	
 	if (mountPoint) {
-		wxTreeListItem cardBranch = treeList->AppendItem(treeList->GetRootItem(), mountPoint);
-		wxTreeListItem networksBranch = treeList->AppendItem(cardBranch, _("Configured Wireles Networks"));
-		struct configured_net_list *configured = fetch_configured_nets();
+		char tempStr[33];
 
-		if (configured->nr == 0) {
-			printf("No wireless networks configured on card\n");
-			return false;
+		wxTreeListItem cardBranch = treeList->AppendItem(treeList->GetRootItem(), _("Eye-Fi Card Mounted at"));
+		treeList->SetItemText(cardBranch, 1, mountPoint);
+
+		wxTreeListItem leaf = treeList->AppendItem(cardBranch, _("Firmware Version"));
+		struct card_firmware_info *info = fetch_card_firmware_info();
+		pstrcpy(tempStr, &info->info);
+		treeList->SetItemText(leaf, 1, tempStr);
+		
+		leaf = treeList->AppendItem(cardBranch, _("MAC Address"));
+		struct mac_address *mac = fetch_mac_address();
+		snprintf(tempStr, sizeof(tempStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+				 mac->mac[0], mac->mac[1], mac->mac[2], mac->mac[3], mac->mac[4], mac->mac[5]);
+		treeList->SetItemText(leaf, 1, tempStr);
+		
+		leaf = treeList->AppendItem(cardBranch, _("Upload Key"));
+		struct card_info_rsp_key *key = fetch_card_upload_key();
+		pstrcpy(tempStr, &key->key);
+		treeList->SetItemText(leaf, 1, tempStr);
+		
+		leaf = treeList->AppendItem(cardBranch, _("Transfer Mode"));
+		enum transfer_mode mode = fetch_transfer_mode();
+		treeList->SetItemText(leaf, 1, transfer_mode_names[mode]);
+		
+		leaf = treeList->AppendItem(cardBranch, _("WiFi Radio"));
+		treeList->SetItemText(leaf, 1, wlan_enabled() ? "enabled" : "disabled");
+		
+		wxTreeListItem endlessBranch = treeList->AppendItem(cardBranch, _("Endless Storage"));
+		int endless = fetch_endless();
+		treeList->SetItemText(endlessBranch, 1, endless >> 7 ? "enabled" : "disabled");
+		if (endless >> 7) {
+			leaf = treeList->AppendItem(endlessBranch, _("Percentage"));
+			sprintf(tempStr, "%d%%", endless % 128);
+			treeList->SetItemText(leaf, 1, tempStr);
 		}
-		printf("configured wireless networks:\n");
+		
+		leaf = treeList->AppendItem(cardBranch, _("Direct Mode SSID"));
+		card_info_cmd(DIRECT_MODE_SSID);
+		struct pascal_string *pStr = (struct pascal_string *)eyefi_response();
+		pstrcpy(tempStr, pStr);
+		treeList->SetItemText(leaf, 1, tempStr);
+
+		leaf = treeList->AppendItem(cardBranch, _("Direct Mode Password"));
+		card_info_cmd(DIRECT_MODE_PASS);
+		pStr = (struct pascal_string *)eyefi_response();
+		pstrcpy(tempStr, pStr);
+		treeList->SetItemText(leaf, 1, tempStr);
+		
+		wxTreeListItem networksBranch = treeList->AppendItem(cardBranch, _("Configured Wireless Networks"));
+		struct configured_net_list *configured = fetch_configured_nets();
+		sprintf(tempStr, "%d", configured->nr);
+		treeList->SetItemText(networksBranch, 1, tempStr);
 		for (int i = 0; i < configured->nr; i++) {
 			struct configured_net *net = &configured->nets[i];
-			wxTreeListItem netBranch = treeList->AppendItem(networksBranch, "ESSID");
-			treeList->SetItemText(netBranch, 1, net->essid);
+			wxTreeListItem netLeaf = treeList->AppendItem(networksBranch, "ESSID");
+			treeList->SetItemText(netLeaf, 1, net->essid);
 		}
 
-	
+		leaf = treeList->AppendItem(cardBranch, _("Card Key"));
+		key = fetch_card_key();
+		pstrcpy(tempStr, &key->key);
+		treeList->SetItemText(leaf, 1, tempStr);
+
+		treeList->Expand(cardBranch);
+		treeList->Expand(endlessBranch);
+		treeList->Expand(networksBranch);
+
 	}
 
 	return true;
